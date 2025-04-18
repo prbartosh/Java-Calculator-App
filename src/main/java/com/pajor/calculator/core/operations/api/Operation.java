f8064c0f6c1a20557cb004ee6b1459785a7215d2
Bartosz Pajor <pajorb4@gmail.com>
2025-04-18 16:32:25 +0200

    new-catalog-structure

---
+++ src/main/java/com/pajor/calculator/core/operations/api/Operation.java
@@ -0,0 +1,6 @@
+package com.pajor.calculator.core.operations.api;
+
+public interface Operation {
+    double apply(double a, double b);
+}
+
---
+++ src/main/java/com/pajor/calculator/core/operations/impl/Addition.java
@@ -0,0 +1,8 @@
+import com.pajor.calculator.core.operations.api.Operation;
+
+public class Addition implements Operation {
+    @Override
+    public double apply(double a, double b) {
+        return a + b;
+    }
+}
